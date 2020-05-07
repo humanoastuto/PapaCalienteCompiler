96605782195e23068bc98fcf48bfbee82cf6b9db
Ricardo <humanoastuto321@gmail.com>
2020-05-07 16:30:59 -0400

    First Commit

---
+++ src/codigo/Principal.java
@@ -0,0 +1,41 @@
+package codigo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        String ruta1 = "../src/codigo/Lexer.flex";
+        String ruta2 = "../src/codigo/LexerCup.flex";
+        String[] rutaS = {"-parser", "Sintax", ".../src/codigo/Sintax.cup"};
+        generar(ruta1, ruta2, rutaS);
+    }
+    public static void generar(String ruta1, String ruta2, String[] rutaS) throws IOException, Exception{
+        File archivo;
+        archivo = new File(ruta1);
+        JFlex.Main.generate(archivo);
+        archivo = new File(ruta2);
+        JFlex.Main.generate(archivo);
+        java_cup.Main.main(rutaS);
+        
+        Path rutaSym = Paths.get("../src/codigo/sym.java");
+        if (Files.exists(rutaSym)) {
+            Files.delete(rutaSym);
+        }
+        Files.move(
+                Paths.get("../sym.java"), 
+                Paths.get("../sym.java")
+        );
+        Path rutaSin = Paths.get("../src/codigo/Sintax.java");
+        if (Files.exists(rutaSin)) {
+            Files.delete(rutaSin);
+        }
+        Files.move(
+                Paths.get("../Sintax.java"), 
+                Paths.get("../src/codigo/Sintax.java")
+        );
+    }
+}
---
+++ src/codigo/Tokens.java
@@ -0,0 +1,33 @@
+package codigo;
+
+public enum Tokens {
+    T_dato,
+    Comillas,
+    ComillaSimple,
+    Caracter,
+    Linea,
+    If,
+    Else,
+    While,
+    For,
+    Cadena,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Op_logico,
+    Op_relacional,
+    Op_booleano,
+    Coma,
+    P_coma,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Identificador,
+    Numero,
+    Cin,
+    Cout,
+    ERROR
+}
